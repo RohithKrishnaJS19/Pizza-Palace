e58001820f1fa57b94822567f478a8ad2888cb4f
RohithKrishnaJS19 <rohithkrishnajs1908@gmail.com>
2026-05-31 15:56:13 +0530

    Shift firebase credential to .env

--- Backend/index.js
+++ Backend/index.js
@@ -11,11 +11,14 @@ app.use(cors())
 app.use(express.json())
 
 // Initialize Firebase Admin
-const admin = require("firebase-admin")
-const serviceAccount = require("./serviceAccountKey.json")
+const admin = require("firebase-admin");
 
 admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount)
+    credential: admin.credential.cert({
+        projectId: process.env.FIREBASE_PROJECT_ID,
+        clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+        privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+    }),
 })
 
 // connecting database
